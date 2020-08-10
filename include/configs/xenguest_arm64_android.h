6c39244d571b477a3548500927f86a93e1211552
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-08-10 08:21:41 +0300

    pvblock: android: Use ab_select to chose and support A/B partitons
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- include/configs/xenguest_arm64_android.h
+++ include/configs/xenguest_arm64_android.h
@@ -22,10 +22,11 @@
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	"bootcmd=bcb load 0 3 pvblock; if bcb test command = boot-recovery; then run boot_pvblock_recovery; else run boot_pvblock; fi;\0" \
 	"recovery_bootargs=androidboot.boot_devices=51712 root=/dev/ram0 ip=dhcp androidboot.hardware=xenvm init=/init console=hvc0 ro rootwait cma=256M@1-2G androidboot.selinux=permissive pvrsrvkm.DriverMode=1 androidboot.android_dt_dir=/proc/device-tree/firmware#1/android/ xt_page_pool=2097152 xt_cma=4194304;\0" \
-	"boot_pvblock_recovery=setenv bootargs $recovery_bootargs; pvblock read ${boot_image_addr}  0x008aa000 0x31fff; bootm ${boot_image_addr} ${boot_image_addr} ${fdt_addr};\0" \
-	"boot_pvblock=run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs;bootm ${boot_image_addr} - ${fdt_addr};\0"\
+	"boot_pvblock_recovery=setenv bootargs $recovery_bootargs; pvblock read ${boot_image_addr}  0x008aa000 0x31fff; run update_bootargs; bootm ${boot_image_addr} ${boot_image_addr} ${fdt_addr};\0" \
+	"boot_pvblock=run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs; run update_bootargs; bootm ${boot_image_addr} - ${fdt_addr};\0"\
 	"fdt_addr=0x48000000\0" \
 	"boot_image_addr=0x90000000\0" \
+	"update_bootargs=ab_select slot pvblock 0#misc; set bootargs ${bootargs}\ androidboot.slot_suffix=_${slot};\0" \
 	"boot_image_part_blk=0x008dc800\0" \
 	"boot_image_length_blk=0xa000\0" \
 	"loadimage=pvblock read ${boot_image_addr} ${boot_image_part_blk} ${boot_image_length_blk};\0" \
