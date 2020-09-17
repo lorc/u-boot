4dd3cdbefce7b4ab7dcdc830395f787988fd2b45
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-09-18 00:45:38 +0300

    pvblock: android: Enable AVB 2.0
    * Enable avb 2.0
    * Refactor cmdline to get rid of hardcode
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- include/configs/xenguest_arm64_android.h
+++ include/configs/xenguest_arm64_android.h
@@ -20,17 +20,24 @@
  * So in at the end bootargs will contain /cosen node + boot_img_hdr->cmdline. */
 
 #define CONFIG_EXTRA_ENV_SETTINGS	\
-	"bootcmd=bcb load 0 3 pvblock; if bcb test command = boot-recovery; then run boot_pvblock_recovery; else run boot_pvblock; fi;\0" \
-	"recovery_bootargs=androidboot.boot_devices=51712 root=/dev/ram0 ip=dhcp androidboot.hardware=xenvm init=/init console=hvc0 ro rootwait cma=256M@1-2G androidboot.selinux=permissive pvrsrvkm.DriverMode=1 androidboot.android_dt_dir=/proc/device-tree/firmware#1/android/ xt_page_pool=2097152 xt_cma=4194304;\0" \
-	"boot_pvblock_recovery=setenv bootargs $recovery_bootargs; pvblock read ${boot_image_addr}  0x008aa000 0x31fff; run update_bootargs; bootm ${boot_image_addr} ${boot_image_addr} ${fdt_addr};\0" \
-	"boot_pvblock=run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs; run update_bootargs; bootm ${boot_image_addr} - ${fdt_addr};\0"\
 	"fdt_addr=0x48000000\0" \
 	"boot_image_addr=0x90000000\0" \
-	"update_bootargs=ab_select slot pvblock 0#misc; set bootargs ${bootargs}\ androidboot.slot_suffix=_${slot};\0" \
-	"boot_image_part_blk=0x008dc800\0" \
-	"boot_image_length_blk=0xa000\0" \
-	"loadimage=pvblock read ${boot_image_addr} ${boot_image_part_blk} ${boot_image_length_blk};\0" \
-	"pvblockboot=run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs;" \
-		"bootm ${boot_image_addr} - ${fdt_addr};\0"
+	"blk_device_id=0\0" \
+	"blk_deivce_if=pvblock\0" \
+	"bootcmd=part number ${blk_deivce_if} ${blk_device_id} misc misc_partition_id; bcb load ${blk_device_id} ${misc_partition_id} ${blk_deivce_if}; if bcb test command = boot-recovery; then echo Booting recovery...; run boot_pvblock_recovery; else echo Booting Android...; run boot_pvblock; fi;\0" \
+	"avb_verify=avb init ${blk_device_id} ${blk_deivce_if}; avb verify _${slot};\0" \
+	"avb_check=if run avb_verify; then                       \
+           echo AVB verification OK. Continue boot; \
+           set bootargs $bootargs $avb_bootargs;    \
+      else                                          \
+           echo AVB verification failed;            \
+           set bootargs $bootargs $avb_bootargs;                                    \
+      fi;\0" \
+	"boot_pvblock=ab_select slot pvblock ${blk_device_id}#misc; run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs; run update_bootargs; run avb_check; bootm ${boot_image_addr} ${boot_image_addr} ${fdt_addr};\0"\
+	"boot_pvblock_recovery=ab_select slot pvblock ${blk_device_id}#misc; run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs; run update_bootargs_recovery; run avb_check; bootm ${boot_image_addr} ${boot_image_addr} ${fdt_addr};\0"\
+	"update_bootargs=set bootargs ${bootargs}\ androidboot.slot_suffix=_${slot} androidboot.force_normal_boot=1;\0" \
+	"update_bootargs_recovery= set bootargs ${bootargs}\ androidboot.slot_suffix=_${slot};\0" \
+	"loadimage=run read_bootimage_params; pvblock read ${boot_image_addr} ${boot_image_part_blk} ${boot_image_length_blk};\0" \
+	"read_bootimage_params=part start pvblock ${blk_device_id} boot_${slot} boot_image_part_blk ; part size pvblock ${blk_device_id} boot_${slot} boot_image_length_blk;\0"
 
 #endif /* __XENGUEST_ARM64_H */
