39708c098eab414f5c6bc07dbf53b7f03560d565
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2020-09-24 15:18:09 +0300

    pvblock: android: Add support of the bootm command
    * Add support bootm command
    * Update cmdline with respect to boot image
    
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

--- include/configs/xenguest_arm64_android.h
+++ include/configs/xenguest_arm64_android.h
@@ -8,11 +8,25 @@
 
 #include <configs/xenguest_arm64.h>
 
+#define CONFIG_SYS_BOOTM_LEN	      (20 * 1024 * 1024)
+
 #undef CONFIG_EXTRA_ENV_SETTINGS
+
+/* NOTE: In case of bootm * boot , u-boot  will set/append
+ * env variable bootargs with boot_img_hdr->cmdline and further overwrite
+ * /chosen node of the fdt. Since /chosen node is the main mechanism to pass cmdline
+ * from Xen domain config to bootloader and Linux kernel, we will prior to all that 
+ * create bootargs variable with /chosen node(using command "fdt get value bootargs /chosen bootargs").
+ * So in at the end bootargs will contain /cosen node + boot_img_hdr->cmdline. */
+
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	"bootcmd=run pvblockboot;\0" \
-	"loadimage=ext4load pvblock 0:7 0x90000000 /Image;\0" \
-	"pvblockboot=run loadimage;" \
-		"booti 0x90000000 - 0x48000000;\0"
+	"fdt_addr=0x48000000\0" \
+	"boot_image_addr=0x90000000\0" \
+	"boot_image_part_blk=0x008dc800\0" \
+	"boot_image_length_blk=0xa000\0" \
+	"loadimage=pvblock read ${boot_image_addr} ${boot_image_part_blk} ${boot_image_length_blk};\0" \
+	"pvblockboot=run loadimage; fdt addr ${fdt_addr}; fdt get value bootargs /chosen bootargs;" \
+		"bootm ${boot_image_addr} - ${fdt_addr};\0"
 
 #endif /* __XENGUEST_ARM64_H */
